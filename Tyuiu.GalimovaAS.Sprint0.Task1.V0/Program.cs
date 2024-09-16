aa642d32cd99f93e481a1c740d1b18d5ae698173
Alina Galimova <Tyuiu.galimovaas@outlook.com>
2024-09-16 23:35:58 +0500

    Task1

---
+++ Tyuiu.GalimovaAS.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.GalimovaAS.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Alina";
+            int age = 18;
+            Console.WriteLine("Hello my name is" + name + "my age is " + age);
+            Console.ReadKey();
+        }
+    }
+}

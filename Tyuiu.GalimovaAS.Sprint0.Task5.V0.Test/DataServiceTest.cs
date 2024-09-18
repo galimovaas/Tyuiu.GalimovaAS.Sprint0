68d129ee8797cdc0766c7aeb1972720bd385fd1e
Alina Galimova <Tyuiu.galimovaas@outlook.com>
2024-09-18 23:14:49 +0500

    Task5

---
+++ Tyuiu.GalimovaAS.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+using Tyuiu.GalimovaAS.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.GalimovaAS.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+             Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    
+    }
+}

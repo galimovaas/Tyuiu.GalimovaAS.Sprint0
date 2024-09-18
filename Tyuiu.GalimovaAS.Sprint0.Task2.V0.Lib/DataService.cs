0b544e66e2fd0b45d263fabe68b99f345269a31a
Alina Galimova <Tyuiu.galimovaas@outlook.com>
2024-09-18 21:11:38 +0500

    Task2

---
+++ Tyuiu.GalimovaAS.Sprint0.Task2.V0.Lib/DataService.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.GalimovaAS.Sprint0.Task2.V0.Lib
+{
+    public class DataService
+    {
+        public static string GetMessage(string name)
+        {
+            return $"Hello, {name}";
+        }
+    }
+}
---
+++ Tyuiu.GalimovaAS.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GalimovaAS.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.GalimovaAS.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Alina";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Hello, Alina", res);
+            
+        }
+    }
+}

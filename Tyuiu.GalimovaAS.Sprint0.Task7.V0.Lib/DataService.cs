a78ca1e4d117f853ce61cac6f06595d6b6b83b9f
Alina Galimova <Tyuiu.galimovaas@outlook.com>
2024-09-19 23:38:24 +0500

    Task7

---
+++ Tyuiu.GalimovaAS.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.GalimovaAS.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] num0ne, int[] numTwo)
+        {
+            int[] resultArray = new int[5];
+            for (var i = 0; i < resultArray.Length; i++)
+            {
+                resultArray[i] = num0ne[i] + numTwo[i];
+            }
+            return resultArray;
+        }
+    }
+}
